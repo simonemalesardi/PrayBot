ba23584a1bd04996bff2b92258e64d1de2f2a1df
Simone Malesardi <simomale98@gmail.com>
2023-02-15 18:08:02 +0100

    aggiunta config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$bot_api_key  = 'INSERT_YOUR_TOKEN';
+$bot_username = 'INSERT_BOT_USERNAME';
+
+$db_credentials = [
+    'host'     => 'localhost',
+    'user'     => 'username',
+    'password' => 'password',
+    'database' => 'database',
+];
